3d991066010a5cb7f1a00596a26d7e5ca52c6b14
PriAC-2025 <priscila.letraspil2005@outlook.com>
2025-05-16 08:52:29 -0300

    Update Untitled-1.jsx

--- petsitting-backend/controllers/petController.js/Untitled-1.jsx
+++ petsitting-backend/controllers/petController.js/Untitled-1.jsx
@@ -8,9 +8,9 @@ exports.createPet = async (req, res) => {
     try {
         const pet = new Pet(req.body);
         await pet.save();
-        res.status(201).json({ message: 'Pet criado com sucesso!', pet });
+        res.status(201).json({ message: 'Cadastro de pet criado com sucesso!', pet });
     } catch (error) {
-        res.status(400).json({ message: 'Erro ao criar Pet', error });
+        res.status(400).json({ message: 'Erro ao criar Cadastro do pet', error });
     }
 };
 
@@ -21,7 +21,7 @@ exports.getPets = async (req, res) => {
         const pets = await Pet.find();
         res.status(200).json(pets);
     } catch (error) {
-        res.status(400).json({ message: 'Erro ao listar Pets', error });
+        res.status(400).json({ message: 'Erro ao listar cadastro dos pets', error });
     }
 };
 
@@ -43,9 +43,9 @@ exports.updatePet = async (req, res) => {
     try {
         const pet = await Pet.findByIdAndUpdate(req.params.id, req.body, { new: true });
         if (!pet) return res.status(404).json({ message: 'Pet não encontrado' });
-        res.status(200).json({ message: 'Pet atualizado com sucesso!', pet });
+        res.status(200).json({ message: 'Cadastro do pet atualizado com sucesso!', pet });
     } catch (error) {
-        res.status(400).json({ message: 'Erro ao atualizar Pet', error });
+        res.status(400).json({ message: 'Erro ao atualizar cadastro do pet', error });
     }
 };
 
@@ -54,9 +54,10 @@ exports.updatePet = async (req, res) => {
 exports.deletePet = async (req, res) => {
     try {
         const pet = await Pet.findByIdAndDelete(req.params.id);
-        if (!pet) return res.status(404).json({ message: 'Pet não encontrado' });
-        res.status(200).json({ message: 'Pet deletado com sucesso!', pet });
+        if (!pet) return res.status(404).json({ message: 'Cadastro do pet não encontrado' });
+        res.status(200).json({ message: 'Cadastro do pet deletado com sucesso!', pet });
     } catch (error) {
-        res.status(400).json({ message: 'Erro ao deletar Pet', error });
+        res.status(400).json({ message: 'Erro ao deletar cadastro do pet', error });
     }
 };
+
