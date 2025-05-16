e5721509fd624d69018c7933d3d4e02e127c0797
PriAC-2025 <priscila.letraspil2005@outlook.com>
2025-05-16 14:02:20 -0300

    Update Untitled-1.jsx

--- petsitting-frontend/src/components/Reservation/ReservationForm.js/Untitled-1.jsx
+++ petsitting-frontend/src/components/Reservation/ReservationForm.js/Untitled-1.jsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from 'react';
 import { getPetsitters, createReservation } from '../../services/api'; 
-import './ReservationForm.css'; //opcional
+import './ReservationForm.css'; 
 
 const ReservationForm = () => {
     const [petsitters, setPetsitters] = useState([]);
