478231077c144bdab889c2da4e8659af794265b4
PriAC-2025 <priscila.letraspil2005@outlook.com>
2025-05-16 08:27:08 -0300

    Update Untitled-1.jsx

--- petsitting-frontend/src/navigation/AppNavigator.js/Untitled-1.jsx
+++ petsitting-frontend/src/navigation/AppNavigator.js/Untitled-1.jsx
@@ -1,7 +1,5 @@
 
 
-//navigation/AppNavigator.js
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Register from '../components/Auth/Register';
