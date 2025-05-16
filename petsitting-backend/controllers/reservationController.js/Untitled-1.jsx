0207f30f54193bc16c77abafb2a0cd4133379812
PriAC-2025 <priscila.letraspil2005@outlook.com>
2025-05-16 08:55:07 -0300

    Update Untitled-1.jsx

--- petsitting-backend/controllers/reservationController.js/Untitled-1.jsx
+++ petsitting-backend/controllers/reservationController.js/Untitled-1.jsx
@@ -8,7 +8,7 @@ exports.createReservation = async (req, res) => {
         await reservation.save();
         res.status(201).json(reservation);
     } catch (error) {
-        res.status(500).json({ message: "Erro ao criar reserva", error });
+        res.status(500).json({ message: "Erro ao criar agendamento", error });
     }
 };
 
